1ef350caf7842a23a332209a6aa2c504c469706d
xubaifuCode <1660866966@qq.com>
2016-09-22 16:23:47 +0800

    完成基础功能

--- js.js
+++ js.js
@@ -1,48 +1,49 @@
 var moveBoundInterval, moveSlidInterval;
 var isStart;
+//recording when key was pressed.
 var left, right;
 //The edges
 var leftEdge, rightEdge, topEdge, bottomEdge;
 //use it to decide direction
 var directX, directY;
 var myBound, mySlid, game_area;
+var maxBoxNum;
 function $(elemId) {
 	return document.getElementById(elemId);
 }
 
 function startGame() {
+	if ($("slid")) {
+		game_area.removeChild(mySlid.obj);
+		game_area.removeChild(myBound.obj);
+	}
+
+	maxBoxNum = 70;
+	generateMap();
+
 	$("menu").style.display = "none";
 	isStart = true;
-	directX = directY = 3;
+	directX = directY = -3;
 	game_area = $("game_area");
-
-	topEdge = 140;
-	bottomEdge = 300;
-	leftEdge = game_area.offsetLeft;
-	rightEdge = leftEdge + game_area.offsetWidth;
-
-
 	myBound = new Ball();
 	mySlid = new Slid();
-	game_area.appendChild(myBound.getBall());
-	game_area.appendChild(mySlid.getSlid());
-
-
+	game_area.appendChild(myBound.obj);
+	game_area.appendChild(mySlid.obj);
+	leftEdge = game_area.offsetLeft;
+	rightEdge = leftEdge + game_area.offsetWidth;
+	topEdge = 140;
 	var tLeftP = leftEdge + parseInt((Math.random() * game_area.offsetWidth));
-	tLeftP = tLeftP > rightEdge - mySlid.width() ? tLeftP - mySlid.width()  : tLeftP;
-
-	mySlid.toPosition(tLeftP, 300);
-	myBound.toPosition(tLeftP, 280);
-
+	tLeftP = tLeftP > rightEdge - 82 ? tLeftP - 82 : tLeftP;
+	mySlid.toPosition(tLeftP, 320);
+	myBound.toPosition(tLeftP, 300);
+	bottomEdge = mySlid.getTop();
 	moveBoundInterval = setInterval("boundMove()", 30);
 	moveSlidInterval = setInterval("slidMove()", 5);
 }
 
 function boundMove() {
-	var bound = $("bound");
-	myBound.toPosition(bound.offsetLeft + directX, bound.offsetTop + directY);
-	
-	if (!resetDirect(myBound.X(), myBound.Y())) {
+	myBound.toPosition(myBound.getLeft() + directX, myBound.getTop() + directY);
+	if (!resetDirect(myBound.getLeft(), myBound.getTop())) {
 		clearInterval(moveBoundInterval);
 		clearInterval(moveSlidInterval);
 		$("menu").style.display = "block";
@@ -51,101 +52,135 @@ function boundMove() {
 }
 
 function generateMap() {
+	//bricks = new Array();
 	var box_area = $("box_area");
-	var str = ""
-	var maxBoxNum = 70;
+	var idStr;
+	var tObj;
 	for (var i = 1; i <= maxBoxNum; i++) {
-		str += "<span class=\"box\" id=\"box_" + i + "\"></span>"
+		idStr = "box_" + i;
+		tObj = new Brick(idStr);
+		box_area.appendChild(tObj.obj);
+		//bricks.push(tObj);
 	}
-	box_area.innerHTML = str;
 }
 
+
+
 function slidMove() {
-	var slid = $("slid");
-	var p;
 	if (left) {
-		p = slid.offsetLeft - 2
-		slid.style.left = p  < leftEdge ? leftEdge : p + "px";
+		var p = slid.offsetLeft - 3;
+		p = p  < leftEdge ? leftEdge : p;
+		mySlid.horizontalMove(p);
 	} else if (right) {
-		p = slid.offsetLeft + 2
-		slid.style.left = p + mySlid.width() > rightEdge ? rightEdge - mySlid.width() : p + "px";
+		var p = slid.offsetLeft + 3;
+		p = p + mySlid.getWidth() > rightEdge ? rightEdge - mySlid.getWidth() : p;
+		mySlid.horizontalMove(p);
 	}
 }
 
 function resetDirect(mX, mY) {
-	var slidRight = mySlid.X() + mySlid.width();
-	if (mX < leftEdge || mX + myBound.width() > rightEdge) {
+	if (mY < topEdge - 5) {
+		return isCrash();
+	}
+
+	if (mX < leftEdge || mX + myBound.getWidth() > rightEdge) {
 		directX *= -1;
 	}
-	if (mY < topEdge) {
+
+	if (mY < 20) {
 		directY *= -1;
-	} else if(mY + myBound.height() > bottomEdge && mX + myBound.width() >= mySlid.X() && mX <= slidRight) {
+	}  else if (mY + myBound.getHeight() > bottomEdge && mX + myBound.getWidth() >= mySlid.getLeft() && mX <= mySlid.getRight()) {
 		directY *= -1;
-	} else if (mY + myBound.height() > bottomEdge) {
+	} else if (mY + myBound.getHeight() > bottomEdge) {
 		return false;
 	}
 	return true;
 }
 
+function isCrash() {
+	var bLeft = myBound.getLeft()  - game_area.offsetLeft;
+	var bRight = bLeft + myBound.getWidth();
+
+	var row = directY < 0 ? Math.floor(myBound.getTop() / 17) - 1 : Math.floor(myBound.getBottom() / 17)  - 2;
+	var col = directX < 0 ? Math.ceil(bLeft / 80) : Math.ceil(bRight / 80);
+	console.log("Row:" + row + ", Col:" + col);
+	var position ="box_" + (row * 10 + col);
+	//console.log(position);
+	if ($(position).style.visibility != "hidden") {
+		$(position).style.visibility = "hidden";
+		//directX *= -1;
+		directY *= -1;
+		maxBoxNum--;
+	}
+	return maxBoxNum != 0;
+}
+
 function Ball() {
-	var bound = document.createElement("div");
-	bound.setAttribute("id", "bound");
-
-	var width = 20;
-	var height = 20;
-
-	this.toPosition = function(toX, toY) {
-		bound.style.left = toX + "px";
-		bound.style.top = toY + "px";
-	}
-
-	this.X = function() {
-		return bound.offsetLeft;
-	}
-
-	this.Y = function() {
-		return bound.offsetTop;
-	}
-
-	this.getBall = function() {
-		return bound;
-	}
-
-	this.width = function() {
-		return width;
-	}
-
-	this.height = function() {
-		return height;
-	}
+	this.createBall = BaseModel;
+	this.createBall("div", null, "bound");
+	delete this.createBall;
 }
 
 function Slid() {
-	var slid = document.createElement("div");
-	slid.setAttribute("id", "slid");
-	
-	var width = 80;
-	var height = 10;
+	this.createSlid = BaseModel;
+	this.createSlid("div", null, "slid");
+	delete this.createSlid;
+}
+
+function Brick(id) {
+	this.createBrick = BaseModel;
+	this.createBrick("span", "box", id);
+	delete this.createBrick;
+	this.isInArea = function(bLeft, bRight, bTop, bBottom) {
+		console.log(bLeft + "" + bRight + "" + bTop + "" + bBottom);
+		console.log(this.obj.id);
+	}
+}
+
+function BaseModel(labelName, className, idName) {
+	this.obj = document.createElement(labelName);
+	if (className != null) {
+		this.obj.setAttribute("class", className);
+	}
+	if (idName != null) {
+		this.obj.setAttribute("id", idName);
+	}
+
+	this.getWidth = function() {
+		return this.obj.offsetWidth;
+	}
+
+	this.getHeight = function() {
+		return this.obj.offsetHeight;
+	}
+
+	this.getLeft = function() {
+		return this.obj.offsetLeft;
+	}
+
+	this.getRight = function() {
+		return this.obj.offsetLeft + this.obj.offsetWidth;
+	}
+
+	this.getTop = function() {
+		return this.obj.offsetTop;
+	}
+
+	this.getBottom = function() {
+		return this.obj.offsetTop + this.obj.offsetHeight;
+	}
+
+	this.horizontalMove = function(toX) {
+		this.obj.style.left = toX + "px";
+	}
+
+	this.verticalMove = function(toY) {
+		this.obj.style.top = toY + "px";
+	}
 
 	this.toPosition = function(toX, toY) {
-		slid.style.left = toX + "px";
-		slid.style.top = toY + "px";
-	}
-
-	this.X = function() {
-		return slid.offsetLeft;
-	}
-
-	this.Y = function() {
-		return slid.offsetTop;
-	}
-
-	this.width = function() {
-		return width;
-	}
-
-	this.getSlid = function() {
-		return slid;
+		this.obj.style.left = toX + "px";
+		this.obj.style.top = toY + "px";
 	}
 }
 
