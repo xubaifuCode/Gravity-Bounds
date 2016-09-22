6a17ec3353f1f2e6d0de164d4e5f7464b92f6cea
xubaifuCode <1660866966@qq.com>
2016-09-22 19:27:24 +0800

    修改界面，优化部分代码

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ var isStart;
 //recording when key was pressed.
 var left, right;
 //The edges
-var leftEdge, rightEdge, topEdge, bottomEdge;
+var leftEdge, rightEdge, topEdge, bottomEdge, minCrashEdge;
 //use it to decide direction
 var directX, directY;
 var myBound, mySlid, game_area;
@@ -17,11 +17,11 @@ function startGame() {
 		game_area.removeChild(mySlid.obj);
 		game_area.removeChild(myBound.obj);
 	}
-	$("show_welcome").style.display = "none";
-	maxBoxNum = 70;
+	$("show_something").style.display = "none";
+	maxBoxNum = 1;
 	generateMap();
 	isStart = true;
-	directX = directY = -2;
+	directX = directY = -1;
 
 	game_area = $("game_area");
 	myBound = new Ball();
@@ -31,12 +31,14 @@ function startGame() {
 
 	leftEdge = game_area.offsetLeft;
 	rightEdge = leftEdge + game_area.offsetWidth;
-	topEdge = 140;
+	topEdge = game_area.offsetTop;
+	minCrashEdge = (maxBoxNum / 10 + 1) * 15 + topEdge;
+	console.log(minCrashEdge);
 
 	//Generated the position of slid and ball.
 	var tLeftP = 0;
 	while(tLeftP < leftEdge || tLeftP > (rightEdge - 100)) {
-		tLeftP = leftEdge + Math.floor(Math.random() * game_area.offsetWidth);
+		tLeftP = leftEdge + Math.floor(Math.random() * 666);
 	}
 
 	mySlid.toPosition(tLeftP, 320);
@@ -45,7 +47,7 @@ function startGame() {
 	bottomEdge = mySlid.getTop();
 
 	//Start to move
-	moveBoundInterval = setInterval("boundMove()", 30);
+	moveBoundInterval = setInterval("boundMove()", 5);
 	moveSlidInterval = setInterval("slidMove()", 5);
 }
 
@@ -53,13 +55,19 @@ function boundMove() {
 	myBound.toPosition(myBound.getLeft() + directX, myBound.getTop() + directY);
 
 	if (!resetDirect(myBound.getLeft(), myBound.getTop())) {
-		clearInterval(moveBoundInterval);
-		clearInterval(moveSlidInterval);
-		$("show_welcome").style.display = "block";
-		$("menu").innerHTML = "again"
-		$("box_area").innerHTML = "";
+		$("hint_info").innerHTML = "Game Over"
 		console.log("Game over.");
+	} else if (maxBoxNum == 0) {
+		$("hint_info").innerHTML = "You Win"
+		console.log("You win.");
+	} else {
+		return;
 	}
+	clearInterval(moveBoundInterval);
+	clearInterval(moveSlidInterval);
+	$("show_something").style.display = "block";
+	$("menu").innerHTML = "again"
+	$("box_area").innerHTML = "";
 }
 
 function generateMap() {
@@ -86,40 +94,40 @@ function slidMove() {
 }
 
 function resetDirect(mX, mY) {
-	if (mY < topEdge - 5) {
-		return isCrash();
-	}
-
 	if (mX < leftEdge || mX + myBound.getWidth() > rightEdge) {
 		directX *= -1;
 	}
 
-	if (mY < 20) {
+	if (mY < topEdge || (mY < minCrashEdge && isCrash())) {
 		directY *= -1;
-	}  else if (mY + myBound.getHeight() > bottomEdge && mX + myBound.getWidth() >= mySlid.getLeft() && mX <= mySlid.getRight()) {
+	} else if (mY + myBound.getHeight() > bottomEdge && mX + myBound.getWidth() >= mySlid.getLeft() && mX <= mySlid.getRight()) {
 		directY *= -1;
 	} else if (mY + myBound.getHeight() > bottomEdge) {
 		return false;
 	}
+	
 	return true;
 }
 
 function isCrash() {
+	var bTop = myBound.getTop() - topEdge;
+	var bBottom = myBound.getBottom() -topEdge;
 	var bLeft = myBound.getLeft()  - game_area.offsetLeft;
 	var bRight = bLeft + myBound.getWidth();
 
-	var row = directY < 0 ? Math.floor(myBound.getTop() / 17) - 1 : Math.floor(myBound.getBottom() / 17)  - 2;
+	var row = directY < 0 ? Math.floor(bTop/ 17) - 1 : Math.floor(bBottom / 17)  - 2;
 	var col = directX < 0 ? Math.ceil(bLeft / 80) : Math.ceil(bRight / 80);
 	console.log("Row:" + row + ", Col:" + col);
 	var position ="box_" + (row * 10 + col);
 	//console.log(position);
-	if ($(position).style.visibility != "hidden") {
-		$(position).style.visibility = "hidden";
-		//directX *= -1;
-		directY *= -1;
+
+	var box = $(position);
+	if (box != null && box.style.visibility != "hidden") {
+		box.style.visibility = "hidden";
 		maxBoxNum--;
+		return true;
 	}
-	return maxBoxNum != 0;
+	return false;
 }
 
 function Ball() {
